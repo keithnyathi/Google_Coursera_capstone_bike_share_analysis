f82117c49d07f1e229acb6387e39d571021b33ca
Tarzan Keith Nyathi <taznuts18@gmail.com>
2026-05-16 11:26:28 +0200

    Started Python analysis with Jupyter Notebooks, added analysis log

--- python_analysis_code/1_loading_data.py
+++ python_analysis_code/1_loading_data.py
@@ -1,3 +1,13 @@
 import pandas as pd
-import numpy as np
+from sqlalchemy import create_engine
 
+# Connect to database
+engine = create_engine('postgresql://postgres:1998@localhost:5432/bike_ride_data')
+
+# Only pull the relevant data for analysis
+query = "SELECT* FROM trips_2025 WHERE ride_length < 1 OR ride_length >= 1440"
+df_errors = pd.read_sql(query,engine)
+
+print("Data loaded successfully")
+
+print(df_errors.head(40))
